caf496123493141dd66af82c3a6731fd582f32f7
RedHat <luxorimartin@gmail.com>
2025-11-28 14:51:00 +0330

    v1.0.3

--- PHShorts/cli.py
+++ PHShorts/cli.py
@@ -28,7 +28,7 @@ BANNER = """
 [bold cyan]║[/]  [bold magenta]╚═╝     ╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝[/]  [bold cyan]║[/]
 [bold cyan]╚═══════════════════════════════════════════════════════╝[/]
             [bold yellow]Download PornHub Shorts with Style![/]
-    [dim]version 1.0.2 • Simple & Light[/]
+    [dim]version 1.0.3 • Simple & Light[/]
 """
 
 
